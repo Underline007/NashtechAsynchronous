1ef66c5b59a3b0ea61516ed12b6c641c583b90b1
Underline007 <99379498+Underline007@users.noreply.github.com>
2024-04-26 12:24:09 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NashtechAsynchronousDay2
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int StartNumber;
+            int EndNumber;
+
+ 
+            Console.Write("Enter Start Number: ");
+            while (!int.TryParse(Console.ReadLine(), out StartNumber))
+            {
+                Console.WriteLine("Invalid");
+                Console.Write("Enter the start number agian: ");
+            }
+
+          
+            Console.Write("Enter End Number: ");
+            while (!int.TryParse(Console.ReadLine(), out EndNumber))
+            {
+                Console.WriteLine("Invalid");
+                Console.Write("Enter the end number agian: ");
+            }
+
+            Console.WriteLine($"Prime numbers between {StartNumber} and {EndNumber}:");
+
+            int[] primeNumbers = await GetPrimeNumbersAsync(StartNumber, EndNumber);
+
+            foreach (int prime in primeNumbers)
+            {
+                Console.WriteLine(prime);
+            }
+        }
+
+        static async Task<int[]> GetPrimeNumbersAsync(int start, int end)
+        {
+            return await Task.Run(() =>
+            {
+                List<int> primesList = new List<int>();
+
+                for (int number = start; number <= end; number++)
+                {
+                    if (IsPrime(number))
+                    {
+                        primesList.Add(number);
+                    }
+                }
+                return primesList.ToArray();
+            });
+        }
+
+
+        static bool IsPrime(int number)
+        {
+            if (number <= 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
