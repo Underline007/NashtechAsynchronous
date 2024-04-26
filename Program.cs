2819fadc9437bef17bd56c3b10c1baa07e1d637e
Underline007 <99379498+Underline007@users.noreply.github.com>
2024-04-26 22:52:59 +0700

    Add Compare of Performance using waitAll

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace NashtechAsynchronousDay2
@@ -7,52 +9,76 @@ namespace NashtechAsynchronousDay2
     {
         static async Task Main(string[] args)
         {
-            int StartNumber;
-            int EndNumber;
-
- 
+            int startNumber;
+            int endNumber;
             Console.Write("Enter Start Number: ");
-            while (!int.TryParse(Console.ReadLine(), out StartNumber))
+            while (!int.TryParse(Console.ReadLine(), out startNumber))
             {
                 Console.WriteLine("Invalid");
-                Console.Write("Enter the start number agian: ");
+                Console.Write("Enter the start number again: ");
             }
 
-          
             Console.Write("Enter End Number: ");
-            while (!int.TryParse(Console.ReadLine(), out EndNumber))
+            while (!int.TryParse(Console.ReadLine(), out endNumber) || endNumber < startNumber)
             {
-                Console.WriteLine("Invalid");
-                Console.Write("Enter the end number agian: ");
+                if (endNumber < startNumber)
+                {
+                    Console.WriteLine("End number should be greater than or equal to start number.");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid");
+                }
+                Console.Write("Enter the end number again: ");
             }
 
-            Console.WriteLine($"Prime numbers between {StartNumber} and {EndNumber}:");
-
-            int[] primeNumbers = await GetPrimeNumbersAsync(StartNumber, EndNumber);
-
-            foreach (int prime in primeNumbers)
+            Console.WriteLine($"Prime numbers between {startNumber} and {endNumber}:");
+            // Performance of Sync Method
+            Stopwatch timeConsumingOfSyncMethod = Stopwatch.StartNew();
+            int[] primeNumbersSync = GetPrimeNumbers(startNumber, endNumber);
+            timeConsumingOfSyncMethod.Stop();
+            Console.WriteLine($"Time of Sync method: {timeConsumingOfSyncMethod.ElapsedMilliseconds}ms");
+            // Performance of Async Method
+            Stopwatch timeConsumingOfAsyncMethod = Stopwatch.StartNew();
+            int[] primeNumbersAsync = await GetPrimeNumbersAsync(startNumber, endNumber);
+            timeConsumingOfAsyncMethod.Stop();
+            Console.WriteLine("Asynchronous method:");
+            Console.WriteLine($"Time of Async method: {timeConsumingOfAsyncMethod.ElapsedMilliseconds}ms");
+            // Display Result
+            foreach (int prime in primeNumbersAsync)
             {
-                Console.WriteLine(prime);
+                Console.Write(prime+", ");
             }
         }
-
+        // Async Method
         static async Task<int[]> GetPrimeNumbersAsync(int start, int end)
         {
-            return await Task.Run(() =>
+            const int range = 1000; 
+            List<Task<int[]>> tasks = new List<Task<int[]>>();
+
+            for (int i = start; i <= end; i += range)
             {
-                List<int> primesList = new List<int>();
+                int startRange = i;
+                int endRange = Math.Min(i + range - 1, end);
 
-                for (int number = start; number <= end; number++)
-                {
-                    if (IsPrime(number))
-                    {
-                        primesList.Add(number);
-                    }
-                }
-                return primesList.ToArray();
-            });
+                tasks.Add(Task.Run(() => CheckPrimeNumbersInRange(startRange, endRange)));
+            }
+            await Task.WhenAll(tasks);
+            return tasks.SelectMany(t => t.Result).ToArray();
         }
-
+        static int[] CheckPrimeNumbersInRange(int start, int end)
+        {
+            return Enumerable.Range(start, end - start + 1)
+                             .Where(IsPrime)
+                             .ToArray();
+        }
+        // Sync Method
+        static int[] GetPrimeNumbers(int start, int end)
+        {
+            return Enumerable.Range(start, end - start + 1)
+                .Where(IsPrime)
+                .ToArray();
+        }
 
         static bool IsPrime(int number)
         {
@@ -60,7 +86,6 @@ namespace NashtechAsynchronousDay2
             {
                 return false;
             }
-
             for (int i = 2; i <= Math.Sqrt(number); i++)
             {
                 if (number % i == 0)
@@ -68,7 +93,6 @@ namespace NashtechAsynchronousDay2
                     return false;
                 }
             }
-
             return true;
         }
     }
